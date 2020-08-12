86b42637320f57139b1ef8b9b8adf6621315274c
Nathan Szurek <nathan.szurek@yahoo.com>
2020-08-12 16:26:41 -0500

    Added search and alpha sort features

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import Filter from './components/Filter';
 import Header from './components/Header';
 import Home from './components/pages/Home';
 import Row from './components/Row';
@@ -9,7 +8,6 @@ function App() {
   return (
     <div>
       <Header />
-      <Filter />
       <Row />
     </div>
   );
